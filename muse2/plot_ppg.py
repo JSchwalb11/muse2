c406e11db573608e08fdb8ff18bb2c267de197e8
jschwalb11 <joey.schwalb@gmail.com>
2020-02-25 15:36:02 -0600

    reading PPG_* and plotting

---
+++ muse2/plot_ppg.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import csv
+import tkinter
+
+x=[]
+y=[]
+
+with open('PPG_recording_2020-02-25-03.30.47.csv', 'r') as csvfile:
+    plots= csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        if (plots.line_num == 1):
+            plt.xlabel = row[0]
+            plt.ylabel = row[1]
+        else:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+
+plt.plot(x,y, marker='o')
+
+plt.title('Data from the CSV File: {0} and {1}'.format(plt.xlabel, plt.ylabel))
+
+plt.show()
